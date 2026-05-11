addba3cd3b2d777ada964920f610d0165da9f3b3
Mahesh Ravi <maheshravi@Maheshs-MacBook-Air.local>
2026-05-11 14:06:00 +0530

    Restored character banner using external URL and updated layout

--- main.js
+++ main.js
@@ -764,9 +764,13 @@ function renderHome() {
 
     moduleDetails.innerHTML = `
         <div class="home-container animate-in">
+            <div class="character-banner">
+                <img src="https://i.ibb.co/tPcjrqR0/characters.jpg" alt="Faculty Characters">
+            </div>
+            <div class="home-divider"></div>
             <div class="home-header-section">
                 <div class="datetime-badge">${dateStr} • ${timeStr}</div>
-                <h1>Welcome Faculty</h1>
+                <h1 class="welcome-title">WELCOME FACULTY</h1>
             </div>
             
             <div class="home-note">
